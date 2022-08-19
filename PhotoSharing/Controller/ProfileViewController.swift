95f7b8502249d14486917213bde61510abad1e1b
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-19 14:54:40 +0800

    新增Storyboard 登入後的主畫面

---
+++ PhotoSharing/Controller/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  PhotoSharing
+//
+//  Created by 陳鈺翔 on 2022/8/19.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
