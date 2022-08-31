99a0319f95c7a2123380d30666b95358fbcf59d0
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-08-31 19:59:48 +0800

    完成，主頁面的貼文讀取跟貼文顯示

--- PhotoSharing/Controller/HomeTableViewController.swift
+++ PhotoSharing/Controller/HomeTableViewController.swift
@@ -12,7 +12,10 @@ import FirebaseStorage
 
 class HomeTableViewController: UITableViewController {
     
-    // 打開相機介面
+    var posts: [Post] = []
+    var isLoadingPost = false
+    
+    // MARK: - 打開相機介面，上傳貼文照片
     // https://github.com/Yummypets/YPImagePicker
     
     @IBAction func openCamera(sender: UIBarButtonItem) {
@@ -39,6 +42,7 @@ class HomeTableViewController: UITableViewController {
             PostService.shared.uploadPostImage(image: photo.image) {
                 
                 picker.dismiss(animated: true, completion: nil)
+                self.loadNewestPosts()
             }
         }
         present(picker, animated: true, completion: nil)
@@ -48,7 +52,79 @@ class HomeTableViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.cellLayoutMarginsFollowReadableWidth = true
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.backgroundColor = UIColor.black
+        refreshControl?.tintColor = UIColor.white
+        refreshControl?.addTarget(self, action: #selector(loadNewestPosts), for: UIControl.Event.valueChanged)
+        
+        loadNewestPosts()
     }
-
     
+    // MARK: - 處理貼文
+    
+    @objc func loadNewestPosts() {
+        
+        isLoadingPost = true
+        
+        PostService.shared.getNewestPosts(start: posts.first?.timestamp, limit: 5) { posts in
+            
+            if posts.count > 0 {
+                self.posts.insert(contentsOf: posts, at: 0)
+            }
+            
+            self.isLoadingPost = false
+            
+            if let _ = self.refreshControl?.isRefreshing {
+                
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.0) {
+                    
+                    self.refreshControl?.endRefreshing()
+                    self.displayNewestPosts(newPosts: posts)
+                }
+            } else {
+                
+                self.displayNewestPosts(newPosts: posts)
+            }
+        }
+    }
+    
+    func displayNewestPosts(newPosts posts: [Post]) {
+        
+        guard posts.count > 0 else {
+            return
+        }
+        
+        var indexPaths: [IndexPath] = []
+        self.tableView.beginUpdates()
+        for i in 0...(posts.count - 1) {
+            
+            let indexPath = IndexPath(row: i, section: 0)
+            indexPaths.append(indexPath)
+        }
+        self.tableView.insertRows(at: indexPaths, with: .fade)
+        self.tableView.endUpdates()
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeTableViewController {
+   
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "datacell", for: indexPath) as! PostsTableViewCell
+        
+        cell.configurePost(post: posts[indexPath.row])
+        return cell
+    }
 }
--- PhotoSharing/View/PostsTableViewCell.swift
+++ PhotoSharing/View/PostsTableViewCell.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class PostsTableViewCell: UITableViewCell {
 
+    var nowpost: Post?
+    
+    @IBOutlet var nameLabel: UILabel!
+
+    @IBOutlet var postImageView: UIImageView! {
+        didSet {
+            postImageView.contentMode = .scaleAspectFill
+        }
+    }
+    
+    @IBOutlet var profileImageView: UIImageView! {
+        didSet {
+            profileImageView.layer.cornerRadius = 15
+            profileImageView.clipsToBounds = true
+            profileImageView.contentMode = .scaleAspectFill
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,6 +36,44 @@ class PostsTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configurePost(post: Post) {
+        
+        nowpost = post
+        
+        self.selectionStyle = .none
+        nameLabel.text = post.username
+
+        postImageView.image = nil
+        
+        if let image = CacheManager.shared.loadFromCache(key: post.imageFileURL) as? UIImage {
+        
+            self.postImageView.image = image
+            
+        } else {
+            
+            if let url = URL(string: post.imageFileURL) {
+                let downloadTask = URLSession.shared.dataTask(with: url) { data, response, error in
+                    
+                    guard let imageData = data else {
+                        return
+                    }
+                    
+                    OperationQueue.main.addOperation {
+                        guard let downloadImage = UIImage(data: imageData) else {
+                            return
+                        }
+                        if self.nowpost?.imageFileURL == post.imageFileURL {
+                            self.postImageView.image = downloadImage
+                        }
+                        self.profileImageView.downloadProfileImage(uid: post.uid)
+                        CacheManager.shared.saveInCache(obj: downloadImage, key: post.imageFileURL)
+                    }
+                }
+                downloadTask.resume()
+            }
+        }
     }
 
 }
